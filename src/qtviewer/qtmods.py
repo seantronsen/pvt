ee0e12c7561dbf76587169c183b7ad7bb39d6f58
Sean Tronsen <sean.tronsen@gmail.com>
2024-03-29 10:28:24 -0600

    [issue-037] -- redesigned trackbar to be numerically stable.

--- src/qtviewer/qtmods.py
+++ src/qtviewer/qtmods.py
@@ -7,15 +7,10 @@ from typing import Optional, Union
 
 class Trackbar(QSlider):
     """
-    A derivation of Qt's QSlider class that allows the slider position to
-    change according to the step size, even when dragged. Why this is not
-    provided as an "in the box" feature for anything except keyboard arrow keys
-    is unknown, especially considering how simple the solution is.
-
-    NEW:
-    Instances of this class are now able to work with floating point or integer
-    numbers. Implementation instantiates an ndarray to hold the specified range
-    with values returned by using the tick position to index into the range.
+    A derivation of Qt's QSlider class that allows use of both integer and
+    floating point ranges. The design of this class also fixes the odd dragging
+    behavior with the original which would occur when users drag the slider out
+    of bounds to other monitors.
     """
 
     def __init__(
@@ -25,14 +20,50 @@ class Trackbar(QSlider):
         step: Union[int, float] = 1,
         init: Optional[Union[int, float]] = None,
     ):
-        assert step < (stop - start), f"error: step value exceeds range ({step=} > {stop - start=})"
+        """
+        Constructor function for the class.
+
+        IMPORTANT: Do note that it is possible to specify parameters that would
+        otherwise yield a fractional number of total steps (total tick mark /
+        slider settings). For example, a range where start=0.1, stop = 0.5, and
+        step=0.3 yields nsteps=2.3 repeating. To ensure the range the user
+        expects is not exceeded, truncated integer division is used to discard
+        the fractional component of nsteps (i.e. nsteps=2). This ensures
+        parameter values remain within the set of values the user expects
+        without exceeding that range. The downside being that in these cases,
+        the specified end boundary for the range will not be included.
+
+        NOTE: The value range will be an integer type if the user specifies
+        integer values for all configuration parameters. If any parameter
+        (excluding `init`) is a floating point value, the range will be
+        converted to floating point type (precision depends on user machine)
+        due to standard numpy calculation type conversion.
+
+        :param start: range start (inclusive)
+        :param stop: range end (inclusive)
+        :param step: step size (default is int(1))
+        :param init: initial active value (important: instantiation fails if
+        this value is not an element of the ndarray used to represent the
+        range).
+        """
+        # set unset optionals
         init_value = init if init is not None else start
-        min_value, max_value = start, stop + step  # np arange excludes final value by default
-        values = np.arange(start=min_value, stop=max_value, step=step)
-        init_value = np.array(init_value, dtype=values.dtype).item()
+
+        # set / check range
+        nrange = stop - start
+        assert step > 0, f"error: step must be a positive real or integer number, received {step}"
+        assert step < nrange, f"error: step value exceeds range ({step=} > {nrange=})"
+
+        # calculate number of steps with truncated integer divison
+        nsteps = ((stop - start) // step) + 1
+        values = np.arange(nsteps, dtype=np.intp) * step
+        values = values + start
+
+        # ensure initial value is a valid setting
         init_index = np.where(np.isclose(values, init_value))[0]
         assert init_index.size != 0, f"error: {init_value=} does not exist in the specified range {values}"
-        init_index = init_index.item()
+
+        # initialize
         super().__init__(Qt.Horizontal)  # pyright: ignore
         self.setFocusPolicy(Qt.StrongFocus)  # pyright: ignore
         self.setTickPosition(QSlider.TickPosition.TicksBelow)
@@ -40,17 +71,27 @@ class Trackbar(QSlider):
         self.setSingleStep(1)
         self.setPageStep(1)
         self.value_range = values
-        self.setValue(init_index)
+        self.setValue(init_index.item())
 
     def value(self) -> int:
         """
-        override to use ndarray range instead.
+        An override of the base class method since the value held by the
+        underlying slider refers to an index in the ndarray range held by
+        instances of this class. Said index is used to return the target value
+        to the caller.
         :return: the value at index referenced by the trackbar position
         """
         index = super().value()
         return self.value_range[index].item()
 
     def mouseMoveEvent(self, ev: QMouseEvent) -> None:
+        """
+        An override of the base class method that eliminates odd behavior that
+        occurs when the user drags the slider to an out of bounds position.
+
+        :param ev: a qt mouse event
+        """
+
         width = self.width()
 
         # determine position
