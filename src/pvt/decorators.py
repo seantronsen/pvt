af2e6beeb73977cb50e51fed5ba7327d483f4945
Sean Tronsen <sean.tronsen@gmail.com>
2025-02-27 16:11:18 -0700

    [issue-8] -- improved decorator definition. fixes gitlab issues 6.

--- src/pvt/decorators.py
+++ src/pvt/decorators.py
@@ -1,41 +1,118 @@
 import time
-from functools import wraps
 import os
-from typing import Callable
+import functools
+from typing import Callable, TypeVar, Any
 
 
-# todo: improve this so it can be attached to any callable object, functions,
-# or methods.
-def perflog(event: str):
-    """
-    Attached to a defined function to wrap with a performance logging feature.
-    Used only when the associated environment variable is true.
+T = TypeVar("T", bound=Callable[..., Any])
 
-    IMPORTANT: Right now this is only intended to be used with display panes.
 
-    IMPORTANT: The specified function must take a **kwargs argument. Nuisances
-    aside, the requirement allows this decorator to remain compatible with
-    instance methods and classic functions alike.
+def perflog(*dargs: Any, **dkwargs: Any) -> Any:
 
-    :param func: routine to decorate
-    """
+    epsilon = 1e-9
+    flag_should_log = os.getenv("VIEWER_DEBUG") == "1"
 
-    def decorate(func: Callable):
+    def decorator(func: T) -> T:
 
-        epsilon = 1e-9
+        label: str = dkwargs.get("label", func.__qualname__)
+        event: str = dkwargs.get("extra_info", "")
 
-        if not os.getenv("VIEWER_DEBUG") == "1":
+        if not flag_should_log:
             return func
 
-        @wraps(func)
-        def wrapper(self, *args, **kwargs):
-            identifier = self.identifier
-            start = time.time()
-            result = func(self, *args, **kwargs)
-            elapsed = time.time() - start
+        @functools.wraps(func)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+
+            # explicitly annotate nonlocals to *appease* the static type checker... sigh.
+            nonlocal label, event
+
+            # check if args[0] is an instance parameter (self) and see if
+            # instance has attr 'identifier'
+            attr_label = "identifier"
+            if args and not isinstance(args[0], type) and hasattr(args[0], attr_label):
+                label = str(getattr(args[0], attr_label))
+
+            start_time = time.perf_counter()
+            result = func(*args, **kwargs)
+            elapsed: float = time.perf_counter() - start_time
+
             print(
-                f"identifier: {identifier.ljust(30)} event: {event.ljust(10)} processing time (s): {elapsed:010.07f} max possible fps: {1 / (elapsed + epsilon): 015.07f}"
+                f"identifier: {label.ljust(30)} "
+                f"{f"event: {event.ljust(10)}" if event else ""} "
+                f"processing time (s): {elapsed:010.07f} "
+                f"updates per second: {1 / (elapsed + epsilon):015.07f}"
             )
             return result
-        return wrapper
-    return decorate
+
+        return wrapper  # type: ignore
+
+    # used without parameters, dargs[0] is the function.
+    if dargs and callable(dargs[0]):
+        return decorator(dargs[0])
+    else:
+        return decorator
+
+
+# note: the implementation above is cleaner, but not the best for python's static typing.
+# the one below is an improvement that removes the errors, but is more
+# complicated and contains more repeated codes.
+#
+# def perf_logger(*dargs: Any, **dkwargs: Any) -> Any:
+#
+#     epsilon = 1e-9
+#     flag_should_log = os.getenv("VIEWER_DEBUG") == "1"
+#
+#     if dargs and callable(dargs[0]):
+#         func = dargs[0]
+#         if not flag_should_log:
+#             return func
+#
+#         @functools.wraps(func)
+#         def wrapper(*args: Any, **kwargs: Any) -> Any:
+#
+#             label = func.__qualname__
+#             if args and not isinstance(args[0], type) and hasattr(args[0], 'identifier'):
+#                 label = str(getattr(args[0], 'name'))
+#
+#             start_time: float = time.perf_counter()
+#             result = func(*args, **kwargs)
+#             elapsed: float = time.perf_counter() - start_time
+#             message = (
+#                 f"identifier: {label.ljust(30)} "
+#                 f"processing time (s): {elapsed:010.07f} "
+#                 f"updates per second: {1 / (elapsed + epsilon):015.07f}"
+#             )
+#
+#             print(message)
+#             return result
+#
+#         return wrapper
+#     else:
+#         label: str | None = dkwargs.get("label", None)
+#
+#         def decorator(func: T) -> T:
+#             if not flag_should_log:
+#                 return func
+#
+#             label: str = label if label is not None else func.__qualname__
+#
+#             @functools.wraps(func)
+#             def wrapper(*args: Any, **kwargs: Any) -> Any:
+#                 start_time: float = time.perf_counter()
+#                 result = func(*args, **kwargs)
+#                 elapsed: float = time.perf_counter() - start_time
+#
+#                 message = (
+#                     f"identifier: {label.ljust(30)} "
+#                     f"event: {event.ljust(10)} "
+#                     f"processing time (s): {elapsed:010.07f} "
+#                     f"updates per second: {1 / (elapsed + epsilon):015.07f}"
+#                 )
+#
+#                 print(message)
+#
+#                 return result
+#
+#             return wrapper  # type: ignore
+#
+#         return decorator
